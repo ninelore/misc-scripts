3d550146eac3546d5b272ca0fed5c104c5864a29
Ingo Reitz <ninelore@protonmail.com>
2022-01-09 16:41:39 +0100

    wine-setcap.sh

---
+++ wine-setcap-fix.sh
@@ -0,0 +1,9 @@
+# This script applies some setcap permissions to wine to allow ACT for FFXIV to work
+
+echo "Applying setcap for wine, wine64 and wineserver"
+
+sudo setcap cap_net_raw,cap_net_admin,cap_sys_ptrace=eip "$(which wine)"
+sudo setcap cap_net_raw,cap_net_admin,cap_sys_ptrace=eip "$(which wine64)"
+sudo setcap cap_net_raw,cap_net_admin,cap_sys_ptrace=eip "$(which wineserver)"
+
+echo "Done"
